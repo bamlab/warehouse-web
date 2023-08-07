506e43b90436ee5ff1f169bd7067709d336047ef
Antoine Cottineau <61667876+antoine-cottineau@users.noreply.github.com>
2023-08-07 12:04:00 +0200

    feat: create a new React app (#1)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        This is a simple React website that is associated with the Warehouse app
+      </h1>
+    </div>
+  );
+}
+
+export default App;
